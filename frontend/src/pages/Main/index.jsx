a073f24b25a7c833f30ec39fc6d945c71119bebf
AlienDev66 <59874434+AlienDev66@users.noreply.github.com>
2020-08-01 15:10:29 +0100

    :heavy_plus_sign: Add styled-components.

--- frontend/src/pages/Main/index.jsx
+++ frontend/src/pages/Main/index.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import { Title } from "./styles";
+
 export default function Main() {
   return (
     <>
-      <h1>Main</h1>
+      <Title error={false}>
+        Main<small>menor</small>{" "}
+      </Title>
     </>
   );
 }
