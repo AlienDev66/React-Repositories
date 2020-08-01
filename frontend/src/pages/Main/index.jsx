66efbdddc6cdfbe9e7b9ddaaefc2a7890750f7b4
AlienDev66 <59874434+AlienDev66@users.noreply.github.com>
2020-08-01 16:10:22 +0100

    :sparkles: Creating form to add new repositories and install axios, and configure axios.

--- frontend/src/pages/Main/index.jsx
+++ frontend/src/pages/Main/index.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import { FaGithubAlt, FaPlus } from "react-icons/fa";
 
-import { Title } from "./styles";
+import api from '../../services/api';
 
-export default function Main() {
-  return (
-    <>
-      <Title>
-          Hello World
-      </Title>
-    </>
-  );
+import { Container, Form, SubmitButton } from "./styles";
+
+export default class Main extends Component {
+  state = {
+    newRepo: "",
+  };
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value })
+  }
+  handleSubmit = e =>{
+    e.preventDefault();
+
+    console.log(this.state.newRepo);
+  }
+
+  render() {
+      const { newRepo } = this.state;
+    return (
+      <>
+        <Container>
+          <h1>
+            <FaGithubAlt />
+            Repositórios
+          </h1>
+          <Form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              placeholder="Adicionar Repositório"
+              value={newRepo}
+              onChange={this.handleInputChange}
+            />
+            <SubmitButton>
+              <FaPlus color="#FFF" size={14} />
+            </SubmitButton>
+          </Form>
+        </Container>
+      </>
+    );
+  }
 }
