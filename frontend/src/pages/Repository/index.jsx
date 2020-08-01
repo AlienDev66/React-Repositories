6003f8c49d1556a54482d27baa48d9738a4198d8
AlienDev66 <59874434+AlienDev66@users.noreply.github.com>
2020-08-01 18:00:15 +0100

    :sparkles: Defining PropTypes.

--- frontend/src/pages/Repository/index.jsx
+++ frontend/src/pages/Repository/index.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
 import api from "../../services/api";
+import PropTypes from 'prop-types';
 
 export default class Repository extends Component {
+
+    static propTypes={
+        match: PropTypes.shape({
+            params: PropTypes.shape({
+                repository: PropTypes.string,
+            }),
+        }).isRequired,
+    }
+
   state = {
     repository: {},
     issues: [],
