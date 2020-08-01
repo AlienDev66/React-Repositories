2f810765d40258c81ad2b41233f9213c27153e9d
AlienDev66 <59874434+AlienDev66@users.noreply.github.com>
2020-08-01 19:02:28 +0100

    :sparkles: Showing Issues.

--- frontend/src/pages/Repository/index.jsx
+++ frontend/src/pages/Repository/index.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import api from "../../services/api";
 import PropTypes from "prop-types";
 
 import Container from "../../components/Container";
-import { Loading, Owner } from "./styles";
+import { Loading, Owner, IssuesList } from "./styles";
 
 export default class Repository extends Component {
   static propTypes = {
@@ -54,7 +54,7 @@ export default class Repository extends Component {
       <>
         <Container>
           <Owner>
-              <Link to="/"> Voltar aos repositórios </Link>
+            <Link to="/"> Voltar aos repositórios </Link>
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -62,6 +62,25 @@ export default class Repository extends Component {
             <h1>{repository.name}</h1>
             <p>{repository.description}</p>
           </Owner>
+
+          <IssuesList>
+            {issues.map((issue) => (
+              <li key={String(issue.id)}>
+                <img src={issue.user.avatar_url} alt={issue.user.login} />
+                <div>
+                  <strong>
+                    <a href={issue.html_url}>{issue.title}</a>
+
+{issue.labels.map(label =>(
+    <span key={String(label.id)} >{label.name}</span>
+))}
+
+                  </strong>
+                  <p>{issue.user.login}</p>
+                </div>
+              </li>
+            ))}
+          </IssuesList>
         </Container>
       </>
     );
--- frontend/src/pages/Repository/styles.js
+++ frontend/src/pages/Repository/styles.js
@@ -1,7 +1,7 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Loading = styled.div`
-  color: #FFF;
+  color: #fff;
   font-size: 30px;
   font-weight: bold;
   display: flex;
@@ -11,36 +11,92 @@ export const Loading = styled.div`
 `;
 
 export const Owner = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-display: flex;
-flex-direction: column;
-align-items: center;
-
-a{
+  a {
     color: #7159c1;
     font-size: 16px;
     text-decoration: none;
-}
+  }
 
-img{
+  img {
     width: 120px;
     border-radius: 50%;
     margin-top: 20px;
+  }
 
-}
-
-h1{
+  h1 {
     font-size: 20px;
     margin-top: 10px;
-}
+  }
 
-p{
+  p {
     margin-top: 5px;
     font-size: 14px;
     color: #666;
     line-height: 1.4;
     text-align: center;
     max-width: 400px;
-}
+  }
+`;
 
+export const IssuesList = styled.ul`
+  padding-top: 30px;
+  margin-top: 30px;
+  border-top: 1px solid #eee;
+  list-style: none;
+
+  li {
+    display: flex;
+    padding: 15px 10px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+
+    & + li {
+      margin-top: 10px;
+    }
+
+    img {
+      width: 36px;
+      height: 36px;
+      border-radius: 50%;
+      border: 2px solid #eee;
+    }
+
+    div {
+      flex: 1;
+      margin-left: 15px;
+
+      strong {
+        font-size: 16px;
+        a {
+          text-decoration: none;
+          color: #333;
+
+          &:hover{
+              color: #7159c1;
+          }
+        }
+
+        span{
+            background: #eee;
+            color: #333;
+            border-radius: 2px;
+            font-size: 12px;
+            font-weight: 600;
+            height: 20px;
+            padding: 3px 4px;
+            margin-left: 10px;
+        }
+
+      }
+      p{
+          margin-top: 5px;
+          font-size: 12px;
+          color: #999;
+      }
+    }
+  }
 `;
