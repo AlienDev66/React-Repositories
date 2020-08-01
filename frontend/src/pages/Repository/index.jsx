831dda043fe5964f5685e66701bd28077fcf46be
AlienDev66 <59874434+AlienDev66@users.noreply.github.com>
2020-08-01 17:46:05 +0100

    :sparkles: Load data to API.

--- frontend/src/pages/Repository/index.jsx
+++ frontend/src/pages/Repository/index.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
+import api from "../../services/api";
 
+export default class Repository extends Component {
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
 
-export default function Repository({ match }) {
-  return (
-    <>
-      <h1>Repository: { decodeURIComponent(match.params.repository)} </h1>
-    </>
-  );
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: "open",
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      loading: false,
+      repository: repository.data,
+      issues: issues.data,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+    return (
+      <>
+        <h1>Repository </h1>
+      </>
+    );
+  }
 }
