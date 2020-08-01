c68b3600eef2ca242e6f0abf3e7aac31c6ec99e8
AlienDev66 <59874434+AlienDev66@users.noreply.github.com>
2020-08-01 17:06:33 +0100

    :sparkles: List repositories.

--- frontend/src/pages/Main/index.jsx
+++ frontend/src/pages/Main/index.jsx
@@ -3,7 +3,7 @@ import { FaGithubAlt, FaPlus, FaSpinner } from "react-icons/fa";
 
 import api from "../../services/api";
 
-import { Container, Form, SubmitButton } from "./styles";
+import { Container, Form, SubmitButton, List } from "./styles";
 
 export default class Main extends Component {
   state = {
@@ -36,7 +36,7 @@ export default class Main extends Component {
   };
 
   render() {
-    const { newRepo, loading } = this.state;
+    const { newRepo, loading, repositories } = this.state;
     return (
       <>
         <Container>
@@ -59,6 +59,15 @@ export default class Main extends Component {
               )}
             </SubmitButton>
           </Form>
+
+          <List>
+              { repositories.map(repository=>(
+                  <li key={repository.name}>
+                      <span> {repository.name} </span>
+                      <a href="" >Detalhes</a>
+                  </li>
+              )) }
+          </List>
         </Container>
       </>
     );
